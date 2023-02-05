962890fc1f65d78a4e72e7b1db14a274dc069339
alenegri1123 <alemnegri84@gmail.com>
2023-02-05 20:24:42 -0300

    CRUD create user

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -2,148 +2,41 @@ const fs = require('fs');
 const path = require('path');
 const model = require("../database/models/User");
 const { Op } = require("sequelize");
+const db = require("../database/models"); 
 
+// CREACION DE USUARIO
+const register = function (req, res) {    
+    res.render('users/register');
+}
 
-//TRAE TODA LA DATA DE USERTABLE
-const userList = async function (req, res) {
-    try {
-        const userData = await model.user.findAll();
-        if (userData.length > 0) {
-             res
-                .status(200)
-                .json({ message: "Connection successful", data: userData });
-        } else {
-            res.status(200).json({ message: "Connection failed", data: [] });
-        }
-    } catch (error) {
-        res.status(404).json({ message: error });
-    }
-};
-
-const detail = async function (req, res) {
-    try {
-        const userData = await model.user.findAll({
-        where: { name: { [Op.like]: `%${req.params.name}%` } },
-        });
-        if (userData.length > 0) {
-            res
-            .status(200)
-            .json({ message: "Connection successful", data: userData });
-        } else {
-        res.status(200).json({ message: "Connection failed", data: [] });
-        }
-    } catch (error) {
-        res.status(404).json({ message: error });
-    }
-};
-
-
-//CREAR USUARIO
+//  GUARDADO DE USUARIO
 const createUser = async function (req, res) {
     try {
-        //   check data has already been created
-        const checkData = await model.user.findAll({
-        where: {
-            [Op.or]: {
-                email: req.body.email,
-                password: req.body.password,
-                },
-            },
-        });
-    if (checkData.length > 0) {
-        res.status(500).json({ message: "email is already in use" });
-    } else {
-        await model.user
-            .create({
-                name: req.body.name,
-                lastName: req.body.lastName,
-                email: req.body.email,
-                phone: req.body.phone,
-                password: req.body.password,
-                date: req.body.date,
-                genre: req.body.genre,
-                token: req.body.name + req.body.password,
-        })
-        .then((result) => {
-            res.status(201).json({
-            message: "user successful created",data: {
-                name: req.body.name,
-                lastName: req.body.lastName,
-                email: req.body.email,
-                phone: req.body.phone,
-                password: req.body.password,
-                date: req.body.date,
-                genre: req.body.genre,
-                },
-
-            });
-        });
-    }
+        await db.User.create({
+            name: req.body.name,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            phone: req.body.phone,
+            password: req.body.password1,
+            password: req.body.password2,
+            birthday: req.body.birthday,
+            genre: req.body.genre
+        });        
     } catch (error) {
-        res.status(404).json({ message: error });
-    }
-};
-
-const register = (req,res) =>{
-    res.render('users/register')
+        console.log("ERROR CREANDO USUARIO", error)
+    }    
+    res.render('/');
 }
 
-const login = (req,res) =>{
-    res.render('/users/login')
-}
+//  LOGIN DE USUARIO
+
+//  EDICION DE USUARIO
+
+//  ACTUALIZACION DE USUARIO
+
+//  BORRADO DE USUARIO
 
 
+module.exports = { register, createUser};
 
-// UPDATE DEL USUARIO
-const userEdit = async function (req, res) {
-    try {
-        await model.user
-            .findAll({ where: { id: req.body.id } })
-            .then(async (result) => {
-                if (result.length > 0) {
-                    await model.user.update(
-                       {
-                           username: req.body.username,
-                           password: req.body.password,
-                           token: req.body.username + req.body.password,
-                        },
-                        { where: { id: req.body.id } }
-                    );
-                    res.status(200).json({
-                        message: "update successful",
-                        data: {
-                        id: req.body.id,
-                        username: req.body.username,
-                        password: req.body.password,
-                        token: req.body.username + req.body.password,
-                        },
-                    });
-                } else {
-                    res.status(500).json({ message: "update failed" });
-                }
-            });
-    } catch (error) {
-        res.status(404).json({ message: error });
-    }
-};
-
-
-// DELETE USUARIO
-const deleteUser = async function (req, res) {
-    try {
-        await model.user
-            .findAll({ where: { id: req.body.id } })
-            .then(async (result) => {
-        if (result.length > 0) {
-            await model.user.destroy({ where: { id: req.body.id } });
-            res.status(200).json({ message: "delete user successfully" });
-        } else {
-            res.status(404).json({ message: "id user not found" });
-            }
-        });
-    } catch (error) {
-        res.status(404).json({ message: error });
-    }
-};
-
-module.exports = { userList, register, createUser, login, detail, userEdit, deleteUser};
+// userList, , login, detail, userEdit, deleteUser
