69f1ecd8c0c36ae7226f70a2f25888688ea1cd61
samoyedo15 <operacionesgrupopsl@gmail.com>
2023-02-13 18:43:58 -0500

    Nada

--- src/routers/usersRouters.js
+++ src/routers/usersRouters.js
@@ -41,7 +41,7 @@ router.get('/register', userController.register);
 router.post('/register', upload.single('avatar'), logDBMiddleware, userController.createUser);
 
 router.get('/login', userController.loginUser)
-router.post('/', verificarUsuario, upload.single('image'), userController.login)
+// router.post('/', verificarUsuario, upload.single('image'), userController.login)
 
 // router.get('/search', userController.searchUser);
 
